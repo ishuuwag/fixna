6d34c2f7dfcc0e789a7201d53d6eecce5fcba61a
Gervasius <gervasius@logicpp.com.na>
2024-04-28 09:18:11 +0200

    fix: get geolocation from browser

--- ui/src/pages/Dashboard/Dashboard.tsx
+++ ui/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Breadcrumb,
   Button,
@@ -11,6 +11,7 @@ import {
   UploadFile,
   UploadProps,
   theme,
+  message,
 } from "antd";
 import { LogoutOutlined, UploadOutlined } from "@ant-design/icons";
 import { useAuthContext } from "@asgardeo/auth-react";
@@ -34,13 +35,18 @@ const Dashboard = () => {
 
   const [form] = Form.useForm();
 
+  const [isSigningOut, setSigningOut] = useState<boolean>(false);
   const [isLoading, setLoading] = useState<boolean>(false);
   const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const [location, setLocation] = useState<GeolocationCoordinates | undefined>(
+    undefined
+  );
 
   const { signOut, getAccessToken } = useAuthContext();
+  const [messageApi, contextHolder] = message.useMessage();
 
   const handleSignOut = () => {
-    setLoading(true);
+    setSigningOut(true);
     signOut();
   };
 
@@ -59,6 +65,7 @@ const Dashboard = () => {
   };
 
   const onFinish = async () => {
+    setLoading(true);
     const t = await getAccessToken();
     const c = getApiConfig(t);
     const api = new DefectsApi(c);
@@ -67,94 +74,160 @@ const Dashboard = () => {
     try {
       const img = fileList[0] as unknown as File;
       const res = await api.createDefect(
-        { description: desc, town: town, longitude: 27, latitude: 22 },
+        {
+          description: desc,
+          town: town,
+          longitude: location?.longitude ?? 22.5649,
+          latitude: location?.latitude ?? 17.0842,
+        },
         img
       );
+      setLoading(false);
       console.log(res);
     } catch (err) {
+      setLoading(false);
       console.log(err);
     }
   };
 
+  const onGeoLocationGranted = (position: GeolocationPosition) => {
+    setLocation(position.coords);
+  };
+  const onGeoLocationError = (err: GeolocationPositionError) => {
+    messageApi.error(err.message);
+  };
+
+  const opts: PositionOptions = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.permissions.query({ name: "geolocation" }).then((result) => {
+        if (result.state === "granted" || result.state === "prompt") {
+          navigator.geolocation.getCurrentPosition(
+            onGeoLocationGranted,
+            onGeoLocationError,
+            opts
+          );
+        } else {
+          messageApi.warning(
+            "Geolocation was denied, application might not work properly"
+          );
+        }
+      });
+    } else {
+      messageApi.warning("Geolocation is not supported by this browser.");
+    }
+  });
+
   return (
-    <Layout>
-      <Header
-        style={{
-          position: "sticky",
-          top: 0,
-          zIndex: 1,
-          width: "100%",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={["1"]}
-          style={{ flex: 1, minWidth: 0 }}
-        >
-          <MenuItem key={1}>Enter defect</MenuItem>
-          <MenuItem key={2}>All defects</MenuItem>
-        </Menu>
-        <Button
-          type="primary"
-          icon={<LogoutOutlined />}
-          loading={isLoading}
-          disabled={isLoading}
-          onClick={() => handleSignOut()}
-          style={{ marginLeft: "auto" }}
-        >
-          Logout
-        </Button>
-      </Header>
-      <Content style={{ padding: "0 48px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
-        </Breadcrumb>
-        <div
+    <>
+      {contextHolder}
+      <Layout>
+        <Header
           style={{
-            padding: 24,
-            minHeight: 380,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
+            position: "sticky",
+            top: 0,
+            zIndex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
           }}
         >
-          <Form
-            {...layout}
-            form={form}
-            name="control-hooks"
-            onFinish={onFinish}
-            style={{ maxWidth: 600 }}
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            style={{ flex: 1, minWidth: 0 }}
           >
-            <Form.Item
-              name="description"
-              label="Description"
-              rules={[{ required: true }]}
+            <MenuItem key={1}>Enter defect</MenuItem>
+            <MenuItem key={2}>All defects</MenuItem>
+          </Menu>
+          <Button
+            type="primary"
+            icon={<LogoutOutlined />}
+            loading={isSigningOut}
+            disabled={isSigningOut}
+            onClick={() => handleSignOut()}
+            style={{ marginLeft: "auto" }}
+          >
+            Logout
+          </Button>
+        </Header>
+        <Content style={{ padding: "0 48px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 380,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Form
+              {...layout}
+              form={form}
+              name="control-hooks"
+              onFinish={onFinish}
+              style={{ maxWidth: 600 }}
             >
-              <Input />
-            </Form.Item>
-            <Form.Item name="town" label="Town" rules={[{ required: true }]}>
-              <Select placeholder="Select your town" allowClear>
-                <Option value="Windhoek">Windhoek</Option>
-              </Select>
-            </Form.Item>
-            <Form.Item name="image" label="Image" rules={[{ required: true }]}>
-              <Upload {...props}>
-                <Button icon={<UploadOutlined />}>Select File</Button>
-              </Upload>
-            </Form.Item>
-            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </Content>
-      <ContentFooter />
-    </Layout>
+              <Form.Item
+                name="description"
+                label="Description"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item name="town" label="Town" rules={[{ required: true }]}>
+                <Select placeholder="Select your town" allowClear>
+                  <Option value="Windhoek">Windhoek</Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                name="longitude"
+                label="Longitude"
+                rules={[{ required: true }]}
+              >
+                <Input disabled value={location?.longitude} />
+              </Form.Item>
+              <Form.Item
+                name="latitude"
+                label="Latitude"
+                rules={[{ required: true }]}
+              >
+                <Input disabled value={location?.latitude} />
+              </Form.Item>
+              <Form.Item
+                name="image"
+                label="Image"
+                rules={[{ required: true }]}
+              >
+                <Upload {...props}>
+                  <Button icon={<UploadOutlined />}>Select File</Button>
+                </Upload>
+              </Form.Item>
+              <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  loading={isLoading}
+                  disabled={isLoading}
+                >
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Content>
+        <ContentFooter />
+      </Layout>
+    </>
   );
 };
 
